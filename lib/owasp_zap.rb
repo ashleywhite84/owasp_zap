b6a1edda3ba0196e891ce216864c231615547c73
Ashley <ashley.white@infrotex.co.uk>
2018-05-18 16:29:19 +0100

    added missing white space

--- lib/owasp_zap.rb
+++ lib/owasp_zap.rb
@@ -34,8 +34,8 @@ module OwaspZap
             @name    = params [:sessionname]   #creation of new sessions with names and also allows these to be saved with this name
             @overwrite = params [:overwrite]   #allows to set overwirte to true or false
             @redirects  = params [:followRedirects]   # allows the accessurl to for redirections if there is a redirection in place on the site.
-            @recurse  =params [:recurse]   # adds recurse to attack to allow it recurse directories
-            @context  =params [:context]   #apply on context to a url
+            @recurse  = params [:recurse]   # adds recurse to attack to allow it recurse directories
+            @context  = params [:context]   #apply on context to a url
             @output = params[:output] || $stdout #default we log everything to the stdout
         end
 cmd_line += if params[:api_key] == true
